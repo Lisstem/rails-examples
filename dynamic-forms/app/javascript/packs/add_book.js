68ec36777441472f4252e05c57a0c492f841ccb6
Thole Goesmann <thole.goesmann@gmx.de>
2022-01-07 15:43:40 +0100

    Remove unneccessary validation from dropdown listener.

--- dynamic-forms/app/javascript/packs/add_book.js
+++ dynamic-forms/app/javascript/packs/add_book.js
@@ -2,29 +2,25 @@ import Rails from "@rails/ujs"
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('#author_id').addEventListener('change', function(event) {
-        if (event.target.value.match(/^\d+$/)) {
-            Rails.ajax({
-                url: `/authors/${event.target.value}.json`,
-                type: "get",
-                success: function (data) {
-                    // get the book drop down
-                    let bookDropdown = document.querySelector('#book_id');
-                    // select all option elements in the dropdown and remove them
-                    bookDropdown.querySelectorAll('option').forEach(o => o.remove());
-                    // for each book the author has written...
-                    data.books.forEach(function (book) {
-                        // create an new option element, ...
-                        let option = document.createElement('option');
-                        // set it's value and text ...
-                        option.value = book.id;
-                        option.innerText = book.name;
-                        // and add it to the dropDown
-                        bookDropdown.appendChild(option);
-                    });
-                }
-            });
-        } else {
-            alert(`Error: event.target.value is not a valid id.`);
-        }
+        Rails.ajax({
+            url: `/authors/${event.target.value}.json`,
+            type: "get",
+            success: function (data) {
+                // get the book drop down
+                let bookDropdown = document.querySelector('#book_id');
+                // select all option elements in the dropdown and remove them
+                bookDropdown.querySelectorAll('option').forEach(o => o.remove());
+                // for each book the author has written...
+                data.books.forEach(function (book) {
+                    // create an new option element, ...
+                    let option = document.createElement('option');
+                    // set it's value and text ...
+                    option.value = book.id;
+                    option.innerText = book.name;
+                    // and add it to the dropDown
+                    bookDropdown.appendChild(option);
+                });
+            }
+        });
     });
 });
